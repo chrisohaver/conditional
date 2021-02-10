ac859c64e21717f7d4005cf84aa0e00f38c594de
Chris O'Haver <cohaver@infoblox.com>
2021-02-10 16:33:45 -0500

    prune out response variables; doc available vars/funcs
    Signed-off-by: Chris O'Haver <cohaver@infoblox.com>

--- README.md
+++ README.md
@@ -1,3 +1,35 @@
+# Conditional
+
+_Conditional_ is an example/experimental CoreDNS plugin allows a user to define
+boolean expressions for use by other CoreDNS plugins/functions.  It currently
+only interfaces with two POC state features, neither of which are part of
+CoreDNS standard:
+
+* CoreDNS Views (requires: https://github.com/chrisohaver/coredns/tree/views) 
+* Conditional forwarding via pluggable _forward_ plugin policies (requires: https://github.com/chrisohaver/coredns/tree/fwd-poliplug).
+
+
+
+### Available Variables
+
+* `type`: type of the request (A, AAAA, TXT, ...)
+* `name`: name of the request (the domain name requested)
+* `class`: class of the request (IN, CH, ...)
+* `proto`: protocol used (tcp or udp)
+* `client_ip`: client's IP address, for IPv6 addresses these are enclosed in brackets: `[::1]`
+* `size`: request size in bytes
+* `port`: client's port
+* `bufsize`: the EDNS0 buffer size advertised in the query
+* `do`: the EDNS0 DO (DNSSEC OK) bit set in the query
+* `id`: query ID
+* `opcode`: query OPCODE
+* `server_ip`: server's IP address; for IPv6 addresses these are enclosed in brackets: `[::1]`
+* `server_port` : client's port
+
+### Available Functions 
+
+* `incidr(ip,cidr)`: returns true if _ip_ is within _cidr_ 
+
 ## CoreDNS Views Example
 
 Requires view-capable CoreDNS (https://github.com/chrisohaver/coredns/tree/views).
--- parameters.go
+++ parameters.go
@@ -2,13 +2,10 @@ package conditional
 
 import (
 	"fmt"
-	"net"
 	"strconv"
 	"strings"
 
 	"github.com/coredns/coredns/request"
-	"github.com/coredns/policy/plugin/pkg/response"
-	"github.com/miekg/dns"
 )
 
 type Parameters struct {
@@ -40,33 +37,6 @@ func makeExtractors() extractorMap {
 		},
 		"port": func(state *request.Request) string {
 			return addrToRFC3986(state.Port())
-		},
-		"rcode": func(state *request.Request) string {
-			rcode := ""
-			rr, ok := state.W.(*response.Reader)
-			if ok && rr.Msg != nil {
-				rcode = dns.RcodeToString[rr.Msg.Rcode]
-				if rcode == "" {
-					rcode = strconv.Itoa(rr.Msg.Rcode)
-				}
-			}
-			return rcode
-		},
-		"rsize": func(state *request.Request) string {
-			rsize := ""
-			rr, ok := state.W.(*response.Reader)
-			if ok && rr.Msg != nil {
-				rsize = strconv.Itoa(rr.Msg.Len())
-			}
-			return rsize
-		},
-		"rflags": func(state *request.Request) string {
-			flags := ""
-			rr, ok := state.W.(*response.Reader)
-			if ok && rr.Msg != nil {
-				flags = flagsToString(rr.Msg.MsgHdr)
-			}
-			return flags
 		},
 		"id": func(state *request.Request) string {
 			return strconv.Itoa(int(state.Req.Id))
@@ -111,48 +81,6 @@ func boolToString(b bool) string {
 	return "false"
 }
 
-// flagsToString checks all header flags and returns those
-// that are set as a string separated with commas
-func flagsToString(h dns.MsgHdr) string {
-	flags := make([]string, 7)
-	i := 0
-
-	if h.Response {
-		flags[i] = "qr"
-		i++
-	}
-
-	if h.Authoritative {
-		flags[i] = "aa"
-		i++
-	}
-	if h.Truncated {
-		flags[i] = "tc"
-		i++
-	}
-	if h.RecursionDesired {
-		flags[i] = "rd"
-		i++
-	}
-	if h.RecursionAvailable {
-		flags[i] = "ra"
-		i++
-	}
-	if h.Zero {
-		flags[i] = "z"
-		i++
-	}
-	if h.AuthenticatedData {
-		flags[i] = "ad"
-		i++
-	}
-	if h.CheckingDisabled {
-		flags[i] = "cd"
-		i++
-	}
-	return strings.Join(flags[:i], ",")
-}
-
 // addrToRFC3986 will add brackets to the address if it is an IPv6 address.
 func addrToRFC3986(addr string) string {
 	if strings.Contains(addr, ":") {
@@ -161,26 +89,3 @@ func addrToRFC3986(addr string) string {
 	return addr
 }
 
-// respIP return the first A or AAAA records found in the Answer of the DNS msg
-func respIP(r *dns.Msg) net.IP {
-	if r == nil {
-		return nil
-	}
-
-	var ip net.IP
-	for _, rr := range r.Answer {
-		switch rr := rr.(type) {
-		case *dns.A:
-			ip = rr.A
-
-		case *dns.AAAA:
-			ip = rr.AAAA
-		}
-		// If there are several responses, currently
-		// only return the first one and break.
-		if ip != nil {
-			break
-		}
-	}
-	return ip
-}
