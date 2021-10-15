3c965c193341cfdfa0aa6fe6471cb15ae456b93e
Chris O'Haver <cohaver@infoblox.com>
2021-10-15 15:12:28 -0400

    fix interface references
    Signed-off-by: Chris O'Haver <cohaver@infoblox.com>

--- policy.go
+++ policy.go
@@ -3,14 +3,14 @@ package conditional
 import (
 	"context"
 
-	"github.com/coredns/coredns/plugin/forward"
+	"github.com/chrisohaver/fwdpolicy"
 	"github.com/coredns/coredns/plugin/metadata"
 	"github.com/coredns/coredns/request"
 )
 
 func (c *Conditional) String() string { return "conditional" }
 
-func (c *Conditional) List(ctx context.Context, p []*forward.Proxy, state *request.Request) []*forward.Proxy {
+func (c *Conditional) List(ctx context.Context, p []*fwdpolicy.Proxy, state *request.Request) []*fwdpolicy.Proxy {
 	params := Parameters{state: state, extractors: c.extractors}
 	for _, r := range c.fwdRules {
 		result, err := r.expr.Eval(params)
@@ -18,7 +18,7 @@ func (c *Conditional) List(ctx context.Context, p []*forward.Proxy, state *reque
 			return nil
 		}
 		if b, ok := result.(bool); ok && b {
-			ups := make([]*forward.Proxy, len(r.upstreams))
+			ups := make([]*fwdpolicy.Proxy, len(r.upstreams))
 			for i, n := range r.upstreams {
 				ups[i] = p[n]
 			}
